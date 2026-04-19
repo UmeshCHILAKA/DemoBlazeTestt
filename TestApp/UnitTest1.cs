a79be87a2fb733473e7c2414b95b4cf4cea62cca
UmeshCHILAKA <umesh.chilaka@outlook.com>
2026-04-19 16:47:50 +0530

    Added tracing for the tests

--- TestApp/UnitTest1.cs
+++ TestApp/UnitTest1.cs
@@ -10,6 +10,18 @@ namespace PlaywrightTests;
 [TestFixture]
 public class ExampleTest : PageTest
 {
+    [SetUp]
+    public async Task Setup()
+    {
+        await Context.Tracing.StartAsync(new()
+        {
+            Title = $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}",
+            Screenshots = true,
+            Snapshots = true,
+            Sources = true
+        });
+    }
+
     [Test]
     public async Task HasTitle()
     {
@@ -29,5 +41,18 @@ public class ExampleTest : PageTest
 
         // Expects page to have a heading with the name of Installation.
         await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
-    } 
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Context.Tracing.StopAsync(new()
+        {
+            Path = Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+             //   "playwright-traces",
+                $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
+            )
+        });
+    }
 }
